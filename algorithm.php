d0a16a1316acd2c5bd20321a8e28c96f63143372
inmated <inmated@yandex.ru>
2017-01-17 23:25:31 +0500

    Add files via upload
    Added Fibonacci range PHP-script.

---
+++ algorithm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <title>Алгоритм</title>
+</head>
+<body>
+  <?php
+    $x = $_GET['x'];
+    echo $x.'<br>';
+    $a = 1; $b = 1;
+
+    while (true) {
+      if ($a <= $x) {
+        if ($a == $x) {
+          echo "Задуманное число входит в числовой ряд.";
+          break;
+        }
+        else {
+          $tmp = $a;
+          $a += $b;
+          $b = $tmp;
+        }
+      }
+      else {
+        echo "Задуманное число НЕ входит в числовой ряд.";
+        break;
+      }
+    }
+  ?>
+</body>
+</html>
