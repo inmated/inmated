a0e62ebc03f4e2b50b8aba86c6e6d177171e8bed
Evgeny <inmated@yandex.ru>
2017-01-17 17:06:19 +0500

    Added profile page.

---
+++ about.php
@@ -0,0 +1,62 @@
+<html>
+  <head>
+    <title>Профиль</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <style>
+      body {
+        font-size: 20px;
+        font-family: Roboto, sans-serif;
+      }
+      table {
+        margin: 100px auto 0;
+        border-spacing: 20px;
+        border: 2px dashed black;
+      }
+      td {
+        max-width: 380px;
+      }
+      th, td:first-child {
+        text-align: right;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+      error_reporting(E_ALL);
+      ini_set('display errors', 1);
+    ?>
+    <?php
+      $name = 'Антон';
+      $age = '21';
+      $mail = 'hs.revision@gmail.com';
+      $address = 'Челябинск';
+      $info = 'Студент. Неплохо знаю HTML и CSS, изучаю JS и PHP. Главная цель - стать Full Stack разработчиком.';
+    ?>
+    <table>
+      <tr>
+        <th>Профиль пользователя Антон:</th>
+      </tr>
+      <tr>
+        <td>Имя</td>
+        <td><?= $name ?></td>
+      </tr>
+      <tr>
+        <td>Возраст</td>
+        <td><?= $age ?> год</td>
+      </tr>
+      <tr>
+        <td>E-Mail</td>
+        <td><?= $mail ?></td>
+      </tr>
+      <tr>
+        <td>Город</td>
+        <td><?= $address ?></td>
+      </tr>
+      <tr>
+        <td>Краткая информация</td>
+        <td><?= $info ?></td>
+      </tr>
+    </table>
+  </body>
+</html>
